85b8a98b5878dc929d249bb6e05197e17532c15e
a.kanibekuly <a.kanibekuly@ait-corp.net>
2023-08-14 06:08:11 +0600

    memory optimization

--- leetcode/2369.Check_if_There_is_a_Valid_Partition_For_The_Array/valid_partition.go
+++ leetcode/2369.Check_if_There_is_a_Valid_Partition_For_The_Array/valid_partition.go
@@ -1,6 +1,30 @@
 package valid
 
 func validPartition(nums []int) bool {
+	dp := make([]bool, 3)
+	dp[0] = true
+
+	for i := range nums {
+		ans := false
+
+		if i > 0 && dp[(i-1)%3] {
+			ans = ans || nums[i] == nums[i-1]
+		}
+
+		if i > 1 && dp[(i-2)%3] {
+			ans = ans || (nums[i] == nums[i-1] && nums[i-1] == nums[i-2])
+			if !ans {
+				ans = ans || (nums[i] == nums[i-1]+1 && nums[i-1] == nums[i-2]+1)
+			}
+		}
+
+		dp[(i+1)%3] = ans
+	}
+
+	return dp[len(nums)%3]
+}
+
+func validPartitionBottomUpDP(nums []int) bool {
 	dp := make([]bool, len(nums)+1)
 	dp[0] = true
 
